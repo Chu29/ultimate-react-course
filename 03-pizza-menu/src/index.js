4cb8c5aa0ab30c907a1da4d9ef87640697313c3f
Chu A. <chu.amk22@gmail.com>
2025-10-24 16:52:14 +0100

    rendering lists in react

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -70,7 +70,13 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza
+
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+      {/* <Pizza
         name="Focaccia"
         ingredients="Bread with italian olive oil and rosemary"
         price={12}
@@ -81,23 +87,22 @@ function Menu() {
         ingredients="Tomato and mozarella"
         price={10}
         photoName="pizzas/margherita.jpg"
-      />
+      /> */}
     </main>
   );
 }
 
 // Component name must start with UpperCase
 function Pizza(props) {
-  console.log(props);
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.name} />
       <div className="pizzas">
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
@@ -106,6 +111,7 @@ function Footer() {
   const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen)
   // hour >= openHour && hour <= closeHour
   //   ? alert("We're currently open")
   //   : alert("Sorry, we're closed!");
