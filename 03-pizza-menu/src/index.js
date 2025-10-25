e8678779e3724aa87f1497c2443e0e4f8f0c34a9
Chu A. <chu.amk22@gmail.com>
2025-10-25 10:01:50 +0100

    extracting jsx into a new component

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -66,6 +66,7 @@ function Header() {
     </header>
   );
 }
+
 function Menu() {
   const pizzas = pizzaData;
   // const pizzas = [];
@@ -121,23 +122,30 @@ function Footer() {
   const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
-  console.log(isOpen);
   // hour >= openHour && hour <= closeHour
   //   ? alert("We're currently open")
   //   : alert("Sorry, we're closed!");
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00
         </p>
       )}
     </footer>
+  );
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open until {props.closeHour}:00. Come visit us or order online
+      </p>
+      <button className="btn">Order</button>
+    </div>
   );
 }
 
