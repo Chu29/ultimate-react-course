f46c981e8504021132d0e6772afd09a0065bb522
Chu A. <chu.amk22@gmail.com>
2026-03-09 12:11:09 +0100

    fetching data in a page

--- the-wild-oasis-website/app/cabins/page.jsx
+++ the-wild-oasis-website/app/cabins/page.jsx
@@ -1,7 +1,16 @@
-export default function Page() {
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+
+  console.log(users);
   return (
     <>
       <h1>Cabins Page</h1>
+      <ul>
+        {users.map((user) => {
+          return <li key={user.id}>{user.name}</li>;
+        })}
+      </ul>
     </>
   );
 }
