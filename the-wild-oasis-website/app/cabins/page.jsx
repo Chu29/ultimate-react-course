4c8fab95bccc1e17d298deeace0d0773a47420ba
Chu A. <chu.amk22@gmail.com>
2026-03-09 12:46:51 +0100

    displaying a loading indicator

--- the-wild-oasis-website/app/cabins/page.jsx
+++ the-wild-oasis-website/app/cabins/page.jsx
@@ -3,8 +3,6 @@ import Counter from "../components/Counter";
 export default async function Page() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const users = await res.json();
-
-  console.log(users);
   return (
     <>
       <h1>Cabins Page</h1>
---
+++ the-wild-oasis-website/app/loading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Loading = () => {
+  return <p>Loading data...</p>;
+};
+
+export default Loading;
